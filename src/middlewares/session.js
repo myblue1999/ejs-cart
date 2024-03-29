ced7ec463d8d6cbe075ff23dac1ad1efdc5c7131
myblue1999 <anfahru1995@gmail.com>
2024-03-29 09:50:55 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/middlewares/session.js
@@ -0,0 +1,26 @@
+import cookieSession from "cookie-session";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const initSession = cookieSession({
+    name: 'session',
+    secret: process.env.SECRET_SESSION,
+    maxAge: 10 * 60 * 1000,
+    keys:['mySecretKey1', 'mySecretKey2']
+});
+
+export const userIsLogged = ((req, res, next) => {
+    res.locals.isLogged = req.session.isLogged;
+    return next();
+  });
+
+export const adminIsLogged = ((req, res, next) => {
+    res.locals.isLoggedAdmin = req.session.isLoggedAdmin;
+    return next();
+});
+
+export const cartItems = ((req, res, next) => {
+    res.locals.cartItems = req.session.cartItems;
+    return next();
+})
